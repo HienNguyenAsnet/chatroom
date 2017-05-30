e5fb33d60f0baf780c445c1adad6d24fbac3d31b
Hien Nguyen <hien.nguyenvan@asnet.com.vn>
2017-05-30 11:04:42 +0700

    Update limit room display on home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   def index
-    @rooms = Room.paginate(page: params[:page], per_page: 1)
+    @rooms = Room.paginate(page: params[:page], per_page: 11)
   end
 end
